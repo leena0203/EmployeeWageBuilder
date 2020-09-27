2c752a8298fd00223f30b27fdb0ccc4d3f0cf040
leena0203 <leenasarode02@gmail.com>
2020-09-27 19:11:48 +0530

    UC5 committed

--- EmployeeCompute.java
+++ EmployeeCompute.java
@@ -1,6 +1,8 @@
 public class EmployeeCompute{
-      	public static final int part_time = 1;
+      	
+	public static final int part_time = 1;
 	public static final int full_time = 2;
+	public static final int working_days = 20;
 	
 	public static void main(String[] args){
 		//constants
@@ -8,22 +10,31 @@ public class EmployeeCompute{
 		//variables
 		int hours_worked = 0;
 		int emp_wage = 0;
-		int emp_check = (int)Math.floor(Math.random()*10)%3;
-		switch(emp_check){
-			case part_time:
-				hours_worked = 5;
-				break;
-			case full_time:
-				hours_worked = 10;
-				break;
-			default:
-				hours_worked = 0;
-		}
+		int total_working_wage = 0;
 		
-		emp_wage = hours_worked * wage_per_hour;
-		System.out.println("Daily emp wage is : " + emp_wage);
-	
+		for(int day=0;day<working_days;day++){
+			int emp_check = (int)Math.floor(Math.random()*10)%3;
+			switch(emp_check){
+				case part_time:
+					hours_worked = 5;
+					break;
+				case full_time:
+					hours_worked = 10;
+					break;
+				default:
+					hours_worked = 0;
+			}
+		
+			emp_wage = hours_worked * wage_per_hour;
+			System.out.println("Daily emp wage is : " + emp_wage);
+			total_working_wage += emp_wage ;
+			
+			
+			
+		}
+		System.out.println("Total emp wage : "+ total_working_wage);
 	}
+		
 
 }
 
